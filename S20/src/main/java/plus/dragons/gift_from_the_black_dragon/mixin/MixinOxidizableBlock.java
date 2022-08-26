43f6fbf5f4962cfc6aec38b227f9e24abc32d706
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-08-26 17:36:41 +0800

    Implement Copper get oxidized next to water.

---
+++ S20/src/main/java/plus/dragons/gift_from_the_black_dragon/mixin/MixinOxidizableBlock.java
@@ -0,0 +1,63 @@
+package plus.dragons.gift_from_the_black_dragon.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Oxidizable;
+import net.minecraft.block.OxidizableBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OxidizableBlock.class)
+public abstract class MixinOxidizableBlock extends Block implements Oxidizable {
+
+    public MixinOxidizableBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    public void injected(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if(hardensOnAnySide(world, pos)){
+            world.setBlockState(pos,tryDegrade(state));
+            ci.cancel();
+        }
+    }
+
+    private BlockState tryDegrade(BlockState state){
+        var result = ((Oxidizable) (Object) this).getDegradationResult(state);
+        if(result.isEmpty()) return state;
+        else return result.get();
+    }
+
+    private static boolean hardensOnAnySide(BlockView world, BlockPos pos) {
+        boolean bl = false;
+        BlockPos.Mutable mutable = pos.mutableCopy();
+        Direction[] var4 = Direction.values();
+        int var5 = var4.length;
+
+        for(int var6 = 0; var6 < var5; ++var6) {
+            Direction direction = var4[var6];
+            BlockState blockState = world.getBlockState(mutable);
+            if (direction != Direction.DOWN || hardensIn(blockState)) {
+                mutable.set(pos, direction);
+                blockState = world.getBlockState(mutable);
+                if (hardensIn(blockState) && !blockState.isSideSolidFullSquare(world, pos, direction.getOpposite())) {
+                    bl = true;
+                    break;
+                }
+            }
+        }
+        return bl;
+    }
+
+    private static boolean hardensIn(BlockState state) {
+        return state.getFluidState().isIn(FluidTags.WATER);
+    }
+}
