b1c6611fa1f6d60532c9b6a085059e14336e1f77
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-08-31 16:12:30 +0800

    Finish porting

---
+++ src/main/java/plus/dragons/wmfo/Faster.java
@@ -0,0 +1,17 @@
+package plus.dragons.wmfo;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("way_much_faster_oxidize")
+public class Faster
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Faster()
+    {}
+
+}
